19cc73f782b047adfdce005022452fa30c92158c
Luke Barrett <gotluke@gmail.com>
2018-09-01 15:38:36 -0600

    Added Image

---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Eye from '../../images/daniil-kuzelev-327645-unsplash.jpg';
+
+const HomePage = () => {
+    return(
+        <div>
+           <img src={Eye} alt="Picture of eye"/>
+        </div>
+    )
+}
+
+export default HomePage;
